dabf29381869b26a981895041bd3d2b0787e6732
João <joaopk.ijui@gmail.com>
2026-08-29 14:45:05 -0300

    [add] funcionalidade de checar digitos repetidos/fora do lugar/pertencentes

--- src/utils/checkDigits.ts
+++ src/utils/checkDigits.ts
@@ -3,17 +3,34 @@ import type { DigitResult } from "../types/digitResult";
 export function checkDigits(guess: string, answer: number) {
   const guessDigits = guess.split("");
   const answerDigits = String(answer).split("");
+
+  const remainingAnswer = [...answerDigits]
  
  
  const results: DigitResult[] = guessDigits.map((digit, index) => 
     {
 
   if (digit === answerDigits[index]) {
-    return "correct";
-  }
 
-  return "absent";
-});
+    const answerIndex = remainingAnswer.indexOf(digit);
+
+      remainingAnswer.splice(answerIndex, 1);
+
+      return "correct";
+  }
+  else if (remainingAnswer.includes(digit)) {
+
+      const answerIndex = remainingAnswer.indexOf(digit);
+
+      remainingAnswer.splice(answerIndex, 1);
+
+      return "present";
+
+}
+
+    return "absent";
+}
+);
 return results;
 }
 
